b1912eee9f2c321ae4004183613b2e8d001680eb
harishchandragiri <giriharishchandra9999@gmail.com>
2025-09-07 22:49:46 +0545

    cart complete

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,8 +1,13 @@
+import Cart from '@/components/Cart'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className=' lg:m-3 lg:mx-5 lg:px-3'>
+      <h2 className='font-semibold text-3xl my-2 px-3 lg:px-0'>Products</h2>
+      <hr className="my-1  border-gray-300" />
+      <Cart/>
+    </div>
   )
 }
 
---
+++ components/Cart.jsx
@@ -0,0 +1,76 @@
+'use client'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+
+const Cart = () => {
+
+  const [users, setUsers] = useState([]); // make it an empty array so .map() won't break
+
+  const API_URL = "http://localhost:1337";
+
+  // Fetch profile data
+  useEffect(() => {
+    axios
+      .get(`${API_URL}/api/transactions?filters[user][id][$eq]=4&populate=*`)
+      .then((res) => {
+        const transactions = res.data.data;
+        if (transactions && transactions.length > 0) {
+          setUsers(transactions);
+        } else {
+          console.warn("No transactions found!!");
+        }
+      })
+      .catch((err) => console.error("Error fetching profile:", err));
+  }, []);
+
+  return (
+    <div>
+              <Table>
+              <TableCaption>A list of your recent transactions( Total Buy ).</TableCaption>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Photo</TableHead>
+                  <TableHead>Product</TableHead>
+                  <TableHead>Quantity</TableHead>
+                  <TableHead>Price</TableHead>
+                  <TableHead className="text-right">Sold Date</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {users.length > 0 ? (
+                  users.map((item) => (
+                    <TableRow key={item.id}>
+                      <TableCell >{item.product}</TableCell>
+                      <TableCell className="font-medium">{item.product}</TableCell>
+                      <TableCell>{item.Quantity}</TableCell>
+                      <TableCell>${item.Price}</TableCell>
+                      <TableCell className="text-right">
+                        {new Date(item.soldDate).toLocaleDateString()}
+                      </TableCell>
+                    </TableRow>
+                  ))
+                ) : (
+                  <TableRow>
+                    <TableCell colSpan={5} className="text-center text-gray-500">
+                      No transactions found ( No Buy Records )
+                    </TableCell>
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+    </div>
+  )
+}
+
+export default Cart
